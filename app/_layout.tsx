19c5bb76e80bcb9a707eb1099623be8303eeac3a
toby77777777 <kibichiitoby4@gmail.com>
2026-01-19 06:56:24 +0300

    Initial Commit

---
+++ app/_layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from "expo-router";
+import {useFonts} from 'expo-font'
+import { ActivityIndicator } from "react-native";
+
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'appFont': require('./../assets/fonts/Montserrat-Light.ttf'),
+    'appFontBold': require('./../assets/fonts/Montserrat-Bold.ttf'),
+    'appFontItalic': require('../assets/fonts/Montserrat-Italic.ttf'),
+    'appFontSemiBold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    'appFontSemiBoldItalic': require('../assets/fonts/Montserrat-SemiBoldItalic.ttf')
+  })
+
+  if(!fontsLoaded) {
+    return <ActivityIndicator />
+  }
+
+  return <Stack />;
+}
