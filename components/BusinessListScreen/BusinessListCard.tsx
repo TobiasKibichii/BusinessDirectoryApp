b914040f87be0199e4a863380fe28dd7dd5b8d37
toby77777777 <kibichiitoby4@gmail.com>
2026-01-21 23:06:04 +0300

    Enabled business list to be shown based on Category Clicked

---
+++ components/BusinessListScreen/BusinessListCard.tsx
@@ -0,0 +1,55 @@
+import { Image, View, Text } from 'react-native'
+import { BusinessType } from '../Homescreen/PopularBusinessList'
+import Colors from '@/services/Colors'
+
+type Props={
+  business:BusinessType
+}
+
+export default function BusinessListCard({business}:Props) {
+  return (
+    <View style={{
+      padding:7,
+      backgroundColor:Colors.WHITE,
+      borderRadius:15,
+      marginTop:10,
+      marginHorizontal:20,
+      display:'flex',
+      flexDirection:'row',
+      gap:10
+    }}>
+      <Image source={{uri: business?.images[0]?.url}} 
+        style={{
+          width:120,
+          height:120,
+          borderRadius:15
+        }}
+      />
+      <View style={{
+        flex:1,
+        gap:5
+      }}>
+        <Text style={{
+          fontFamily:'appFontBold',
+          fontSize:18,
+        }}>{business?.name}</Text>
+        <Text style={{
+          fontFamily:'appFontBold',
+          fontSize:15,
+          color:Colors.GRAY
+        }}>{business?.address}</Text>
+        <View style={{
+          flex:1,
+          flexDirection:'row',
+          gap:5
+        }}>
+          <Image source={require('../../assets/images/star.png')} style={{
+            width:20,
+            height:20
+          }}/>
+          <Text>4.3/5</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
